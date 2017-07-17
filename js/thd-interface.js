a4fb353b12174b249bdb0fb3b9b9e1c74a4726a5
Minh Phuong <mphuong@kent.edu>
2017-07-17 16:53:19 -0700

    slider works

--- js/thd-interface.js
+++ js/thd-interface.js
@@ -22,4 +22,16 @@ $(document).ready(function(){
     $("#outboundUrban1").attr("value",100-parseInt(slideEvt.value) + " %");
   });
 
+  // $("#generate").click(function() {
+  //  $("#proto").append("<tr><td>0-1</td><td><input type='number' id='aadtRural1' name='aadtRural1' class='form-control'></td>
+  //    <td><input id='inboundRural1' data-slider-id='ex1Slider' type='text' data-slider-min='0' data-slider-max='100' data-slider-step='1' data-slider-value='48'/><span id='inboundRural1Value'></span> %
+  //    </td>
+  //    <td><input type='text' id='outboundRural1' name='outboundRural1' class='form-control' disabled=true></td>
+  //    <td><input type='number' id='aadtUrban1' name='aadtUrban1' class='form-control'></td>
+  //    </td><td><input id='inboundUrban1' data-slider-id='ex1Slider' type='text' data-slider-min='0' data-slider-max='100' data-slider-step='1' data-slider-value='47'/><span id='inboundUrban1Value'></span> %</td>
+  //    <td><input type='text' id='outboundUrban1' name='outboundUrban1' class='form-control' disabled=true></td>
+  //  </tr>"
+ );
+  })
+
 })
