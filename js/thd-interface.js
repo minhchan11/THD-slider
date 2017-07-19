fa157d23e5d216bfe16d6e24575873916ed5d332
Minh Phuong <mphuong@kent.edu>
2017-07-19 11:10:29 -0700

    Slider takes default value

--- js/thd-interface.js
+++ js/thd-interface.js
@@ -1,37 +1,33 @@
-$(document).ready(function(){
+var trafficDistDefaults = require('./../js/thd.js').trafficDistDefaults;
 
-  //rural
-  $("#aadtRural0").attr("value", "0.8");
-  var inboundRural0Slider = $("#inboundRural0").slider();
-  var inboundRural0 = inboundRural0Slider.slider('getValue');
-  $("#inboundRural0Value").text(inboundRural0);
-  $("#outboundRural0").attr("value", 000-parseInt(inboundRural0) + " %");
-  $("#inboundRural0").on("slide", function(slideEvt) {
-  	$("#inboundRural0Value").text(slideEvt.value);
-    $("#outboundRural0").attr("value",100-parseInt(slideEvt.value) + " %");
+$(document).ready(function(){
+  var template = '{#.}<tr>'+
+                    '<td>{$idx} - {@math key="{$idx}" method="add" operand="1"/}</td>' +
+                    '<td>'+
+                      '<input id="traffic_' + '{$idx}" type="text" data-slider-min="0" data-slider-max="10" data-slider-step="0.01" data-slider-value={@math key="{.}" method="multiply" operand="100"/} />' +
+                    '</td>' +
+                    '<td>' +
+                    '<input type="hidden" id="trafficDefault_'+ '{$idx}" value={@math key="{.}" method="multiply" operand="100"/}>' +
+                    '<input type="text" id="trafficValue_'+'{$idx}" class="form-control" disabled=true>' +
+                    '</td>'+
+                '</tr>{/.}';
+
+  var dataTemplate = dust.compile(template, "trafficDistTemplate");
+
+  // load the template into the Dust cache
+  dust.loadSource(dataTemplate);
+
+  dust.render("trafficDistTemplate", trafficDistDefaults, function(err, out) {
+    $("#trafficTable").find('tbody').append(out);
   });
 
-  //urban
-  // $("#aadtUrban1").attr("value", "1.2");
-  // var inboundUrban1Slider = $("#inboundUrban1").slider();
-  // var inboundUrban1 = inboundUrban1Slider.slider('getValue');
-  // $("#inboundUrban1Value").text(inboundUrban1);
-  // $("#outboundUrban1").attr("value", 100-parseInt(inboundUrban1) + " %");
-  // $("#inboundUrban1").on("slide", function(slideEvt) {
-  // 	$("#inboundUrban1Value").text(slideEvt.value);
-  //   $("#outboundUrban1").attr("value",100-parseInt(slideEvt.value) + " %");
-  // });
-
-  // $("#generate").click(function() {
-  //  $("#proto").append("<tr><td>0-1</td><td><input type='number' id='aadtRural1' name='aadtRural1' class='form-control'></td>
-  //    <td><input id='inboundRural1' data-slider-id='ex1Slider' type='text' data-slider-min='0' data-slider-max='100' data-slider-step='1' data-slider-value='48'/><span id='inboundRural1Value'></span> %
-  //    </td>
-  //    <td><input type='text' id='outboundRural1' name='outboundRural1' class='form-control' disabled=true></td>
-  //    <td><input type='number' id='aadtUrban1' name='aadtUrban1' class='form-control'></td>
-  //    </td><td><input id='inboundUrban1' data-slider-id='ex1Slider' type='text' data-slider-min='0' data-slider-max='100' data-slider-step='1' data-slider-value='47'/><span id='inboundUrban1Value'></span> %</td>
-  //    <td><input type='text' id='outboundUrban1' name='outboundUrban1' class='form-control' disabled=true></td>
-  //  </tr>"
- // );
- //  })
-
+  for (var i = 0; i < 24; i++) {
+    var trafficSlider = $("#traffic_"+i).slider();
+    var trafficDefault = (parseFloat($("#trafficDefault_"+i).val())).toFixed(2);
+    $("#trafficValue_"+i).attr("value", trafficDefault);
+    $("#traffic_"+i).on("slide", function(slideEvt) {
+      var currentIndex = ((slideEvt.currentTarget.id).split("_").pop());
+      $("#trafficValue_"+currentIndex).attr("value", slideEvt.value);
+    });
+  }
 })
