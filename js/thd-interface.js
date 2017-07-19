3604ee685edf6a5c71e454417a0a0e8ad9a4d682
Minh Phuong <mphuong@kent.edu>
2017-07-19 12:57:05 -0700

    Set SCSS for selection

--- js/thd-interface.js
+++ js/thd-interface.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
   var template = '{#.}<tr>'+
                     '<td>{$idx} - {@math key="{$idx}" method="add" operand="1"/}</td>' +
                     '<td>'+
-                      '<input id="traffic_' + '{$idx}" type="text" data-slider-min="0" data-slider-max="10" data-slider-step="0.01" data-slider-value={@math key="{.}" method="multiply" operand="100"/} />' +
+                      '<input id="traffic_' + '{$idx}" type="text" data-slider-id="trafficSlider_'+'{$idx}" data-slider-min="0" data-slider-max="10" data-slider-step="0.01" data-slider-value={@math key="{.}" method="multiply" operand="100"/} />' +
                     '</td>' +
                     '<td>' +
                     '<input type="hidden" id="trafficDefault_'+ '{$idx}" value={@math key="{.}" method="multiply" operand="100"/}>' +
@@ -23,9 +23,13 @@ $(document).ready(function(){
 
   for (var i = 0; i < 24; i++) {
     var currentIndex;
+    //instantiate slider
     var trafficSlider = $("#traffic_"+i).slider();
+    //get value from hidden input field
     var trafficDefault = (parseFloat($("#trafficDefault_"+i).val())).toFixed(2);
+    //Set visible input field to default value
     $("#trafficValue_"+i).attr("value", trafficDefault);
+    //Create two way binding between input field and slider
     $("#trafficValue_"+i).on("change", function() {
       currentIndex = (this.id).split("_").pop();
     $("#traffic_"+currentIndex).slider('setValue', this.value);
